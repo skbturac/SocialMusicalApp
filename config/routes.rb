63d29df4318113430d7bb11a5e1f58ec666c1ec3
magic8baller <lturis@gmail.com>
2018-11-12 17:30:43 -0500

    updated some sessions stuff

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   get  '/contact', to: 'homes#contact'
   get '/users', to: 'users#index'
   get  '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :comments
   resources :posts
   resources :users
