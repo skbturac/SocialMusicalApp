94134b34fc97c5aa7768857c1aa082143c4d7ea9
github username <github email address>
2018-11-14 11:40:36 -0500

    Commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
-  before_action :current_user, only: [:show]
+  before_action :current_user, only: [:show, :new]
 
     def index
       @posts = Post.all
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
     end
 
     def profile
+      
     end
 
     def new
